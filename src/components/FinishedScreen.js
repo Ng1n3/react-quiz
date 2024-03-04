e7b525ee6134e986f27cc955d72158e563bfc92d
ng1n3 <code.with.muyiwa@gmail.com>
2024-03-04 18:48:06 +0100

    add emoji depending on scores

--- src/components/FinishedScreen.js
+++ src/components/FinishedScreen.js
@@ -1,10 +1,22 @@
-function FinishedScreen({points, maxPossiblePoints}) {
-  const percentage = points/maxPossiblePoints * 100;
+function FinishedScreen({ points, maxPossiblePoints }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🥈";
+  if (percentage >= 60 && percentage < 80) emoji = "🥉";
+  if (percentage >= 0 && percentage < 60) emoji = "😑";
+  if (percentage === 0) emoji = "🫤";
+
   return (
-    <p className="result">
-      You scored <strong>{points}</strong> out of {maxPossiblePoints} ({Math.ceil(percentage)}%)
-    </p>
-  )
+    <>
+      <p className="result">
+        <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: X points)</p>
+    </>
+  );
 }
 
-export default FinishedScreen
+export default FinishedScreen;

434a83fa46ea4c203603dac243b1b1df30fef91d
ng1n3 <code.with.muyiwa@gmail.com>
2024-03-04 09:42:52 +0100

    useReducer to create question and status state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import { useEffect } from "react";
 import Header from "./Header";
+import Main from "./Main.js";
+import { useReducer } from "react";
+
+const initalState = {
+  question: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 export default function App() {
-  return <div className="app">
-    <Header/>
+  const [state, dispatch] = useReducer(reducer, initalState);
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+  return (
+    <div className="app">
+      <Header />
 
-    <main>
-      <p>1 / 15</p>
-      <p>Questions?</p>
-    </main>
-  </div>
+      <Main>
+        <p>1 / 15</p>
+        <p>Questions?</p>
+      </Main>
+    </div>
+  );
 }

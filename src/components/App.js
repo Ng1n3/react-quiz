42f28938bc8532378ae1d2db39628914da9b7ac8
ng1n3 <code.with.muyiwa@gmail.com>
2024-03-04 19:03:56 +0100

    emoji based on score and highscore feature

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,7 @@ const initalState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -28,13 +29,16 @@ function reducer(state, action) {
         questions: action.payload,
         status: "ready",
       };
+
     case "dataFailed":
       return {
         ...state,
         status: "error",
       };
+
     case "start":
       return { ...state, status: "active" };
+
     case "newAnswer":
       const question = state.questions.at(state.index);
       return {
@@ -45,11 +49,17 @@ function reducer(state, action) {
             ? state.points + question.points
             : state.points,
       };
+
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
 
     case "finish":
-      return { ...state, status: "finished" };
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
 
     default:
       throw new Error("Action unknown");
@@ -57,7 +67,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(
     reducer,
     initalState
   );
@@ -110,6 +120,7 @@ export default function App() {
           <FinishedScreen
             points={points}
             maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
           />
         )}
       </Main>

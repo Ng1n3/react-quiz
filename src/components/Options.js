c8399e7381fc0def75a5d2a990907f38217c47c1
ng1n3 <code.with.muyiwa@gmail.com>
2024-03-04 17:25:09 +0100

    get point for correct option, select an option

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,14 +1,24 @@
-function Options() {
+function Options({ question, answer, dispatch }) {
+  const hasAnswered = answer !== null;
   return (
-
-      <div className="options">
-        {question.options.map((option) => (
-          <button className="btn btn-option" key={option}>
-            {option}
-          </button>
-        ))}
-      </div>
-
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={answer !== null}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
   );
 }
 
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,12 @@
-function Question({ question }) {
+import Options from "./Options";
+
+function Question({ question, dispatch, answer }) {
   console.log(question);
 
   return (
     <div>
       <h4>{question.question}</h4>
-
+      <Options question={question} dispatch={dispatch} answer={answer}/>
     </div>
   );
 }
